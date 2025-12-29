3fda6ffd98d34f577ca92db03be7a84df35609bd
VikasVerma-15 <97469119+VikasVerma-15@users.noreply.github.com>
2025-12-29 17:36:02 +0530

    Add state management for selected option in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import ChildComponent1 from "./ChildComponent1";
+import ChildComponent2 from "./ChildComponent2";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [selectedOption, setSelectedOption] = useState("");
 
-export default App
+  return (
+    <div className="parent" style={{ backgroundColor: "lime", padding: "20px" }}>
+      <h2>Parent Component</h2>
+
+      {/* Child Components */}
+      <ChildComponent1 setSelectedOption={setSelectedOption} />
+      <ChildComponent2 setSelectedOption={setSelectedOption} />
+
+      <p>Selected Option: {selectedOption}</p>
+    </div>
+  );
+};
+
+export default App;
